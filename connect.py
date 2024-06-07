bbaab8138e0aa6d93fe1f4577b45e09bfdbb828e
Martin Mladenov <m.mladenov@student.tudelft.nl>
2024-06-07 19:30:18 +0200

    Implement connection code

--- connect.py
+++ connect.py
@@ -2,25 +2,72 @@
 
 import paramiko
 import socket
-import time
+import sys
+import json
+import multiprocessing
 
 key = paramiko.rsakey.RSAKey.generate(2048)
 
-while True:
+socket.setdefaulttimeout(10)
+
+def connect_to_host(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((ip, port))
+        transport = paramiko.Transport(sock)
+    except Exception as e:
+        sock.close()
+        return 'could_not_connect', str(e)
 
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('localhost', 2222))
-
-        transport = paramiko.Transport(sock)
         transport.connect()
-        
-        transport.auth_publickey('testuser', key)
-        
-        print('it worked!')
+        r = transport.auth_publickey('root', key)
+        if r:
+            return 'requires_more_authentication', str(r)
+        return 'key_accepted', None
+    except paramiko.ssh_exception.BadAuthenticationType as e:
+        return 'bad_authentication_type', str(e.allowed_types)
     except paramiko.ssh_exception.AuthenticationException:
-        print('nope')
+        return 'key_rejected', None
     except Exception as e:
-        print(e)
+        return 'could_not_connect', str(e)
+    finally:
+        transport.close()
+        sock.close()
 
-    time.sleep(5)
+
+def process_host(host):
+    ip = host['ip']
+
+    res, msg = connect_to_host(ip, 22)
+
+    host['connection_result'] = res
+    host['connection_details'] = msg
+
+    with curr.get_lock():
+        curr.value += 1
+        print(f"[{curr.value}/{total}] Scanned {ip}: {res}, {msg}", file=sys.stderr)
+
+    return host
+
+def worker_setup(c, t):
+    global curr, total
+    curr, total = c, t
+
+
+def main():
+    filename = sys.argv[1]
+    file = open(filename)
+    hosts = json.load(file)
+    total = len(hosts)
+    print(f'Loaded {total} hosts', file=sys.stderr)
+
+    curr = multiprocessing.Value('i', 0)
+    pool = multiprocessing.Pool(initializer=worker_setup, initargs=[curr, total], processes=64) # connect to 64 hosts at a time
+    results = pool.map(process_host, hosts)
+
+    print(json.dumps(results, indent=4))
+
+
+if __name__ == '__main__':
+    main()
